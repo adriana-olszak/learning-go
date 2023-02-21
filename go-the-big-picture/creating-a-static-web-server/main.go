62d54caa1b3ead48a13d2bca55e614bff3c6d5ee
adriana-olszak <adriana.olszak@gmail.com>
2023-02-21 10:37:27 +0100

    reading parameters from the query string

--- go-the-big-picture/creating-a-static-web-server/main.go
+++ go-the-big-picture/creating-a-static-web-server/main.go
@@ -8,7 +8,14 @@ import (
 func main() {
 	// functions are first class citiziens in Go, you can create it on the fly
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte("Hello World")) // []byte is needed bc writies always work with slices of bytes. We convert String into Byte using []byte
+		names := r.URL.Query()["name"]
+		var name string
+
+		if len(names) == 1 {
+			name = names[0]
+		}
+
+		w.Write([]byte("Hello " + name)) // []byte is needed bc writies always work with slices of bytes. We convert String into Byte using []byte
 	})
 	err := http.ListenAndServe(":3000", nil)
 
@@ -22,6 +29,6 @@ func main() {
 // go build .
 // # go-the-big-picture/creating-a-static-web-server
 // ./main.go:10:18: cannot use '/' (untyped rune constant 47) as string value in argument to http.HandleFunc
-// AGAIN DUE TO VSC NOT SAVED. Strangely enough I did set it to save on window loosing focus. 
-// Looks like Hotkey Warp terminal does not cause it loos focus? 
-// Setting it up so it always saves. 
+// AGAIN DUE TO VSC NOT SAVED. Strangely enough I did set it to save on window loosing focus.
+// Looks like Hotkey Warp terminal does not cause it loos focus?
+// Setting it up so it always saves.
